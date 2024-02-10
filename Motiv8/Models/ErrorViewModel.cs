5bb826c91c10983c2ed040af3f32f6860a85ef51
Asante Laryea Akrong <kal79@byu.edu>
2024-02-09 22:38:54 -0700

    Add project files.

---
+++ Motiv8/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Motiv8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
